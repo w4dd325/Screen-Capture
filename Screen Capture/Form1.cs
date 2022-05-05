e2ff64a2be651c5a37825593f43dd9c496b3aa84
w4dd325 <w4dd325@gmail.com>
2022-05-05 12:14:43 +0100

    Updates

--- Screen Capture/Form1.cs
+++ Screen Capture/Form1.cs
@@ -29,21 +29,24 @@ namespace Screen_Capture
         {
             try
             {
-            //Creating a new Bitmap object
+                //Creating a new Bitmap object
                 //Bitmap captureBitmap = new Bitmap(1024, 768, PixelFormat.Format32bppArgb);
+
+                //40 40 is the height and width I want to capture, change as needed
                 Bitmap captureBitmap = new Bitmap(40, 40, PixelFormat.Format32bppArgb);
                 //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
-            //Creating a Rectangle object which will
-            //capture our Current Screen
+                //Creating a Rectangle object which will
+                //capture our Current Screen
                 //Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+                //363, 180, 40, 40 is the area I want to capture, change as needed
                 Rectangle captureRectangle = new Rectangle(363, 180, 40, 40);
-            //Creating a New Graphics Object
+                //Creating a New Graphics Object
                 Graphics captureGraphics = Graphics.FromImage(captureBitmap);
-            //Copying Image from The Screen
+                //Copying Image from The Screen
                 captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-            //Saving the Image File.
+                //Saving the Image File.
                 captureBitmap.Save(@"C:\Users\James\Downloads\Capture.jpg", ImageFormat.Jpeg);
-            //Displaying the Successfull Result
+                //Displaying the Successfull Result
                 //MessageBox.Show("Screen Captured");
             }
             catch (Exception ex)
